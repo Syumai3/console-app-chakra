5b94de9180cc6a8b76b20931d421abbb29a5444c
s.umeda <syuhei19911212@gmail.com>
2024-01-10 23:02:35 +0900

    初期設定を実施

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { theme } from "././theme/theme";
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <h1>テスト</h1>
+    </ChakraProvider>
+  );
+}
+
+export default App;

39515f561ae3fbf2a71db98a746e0abaca47681e
s.umeda <syuhei19911212@gmail.com>
2024-01-14 11:58:07 +0900

    ユーザー一覧機能を実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,32 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect } from "react";
 import { UserCard } from "../organisms/user/userCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https:source.unsplash.com/randam"
-          userName="じゃけ"
-          fullName="じゃけえ"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https:source.unsplash.com/randam"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
